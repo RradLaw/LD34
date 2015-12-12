3ae5b93f572b2dd66d1a5bcdde4aa55cb250b74d
Jarrad Law <jarrad.law@gmail.com>
2015-12-12 18:36:54 +1030

    Added rotation with key press. Using assets from p5play for now.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,22 @@
+var flower;
+var direction = 0;
+
 function setup() {
-  createCanvas(1280,720);
-  createSprite(400, 200, 50, 50);
+	createCanvas(1280,720);
+	flower = createSprite(600, 620, 50, 100);
+	flower.addAnimation("floating", "assets/ghost_standing0001.png", "assets/ghost_standing0007.png");
 }
 
 function draw() {
-  background(127,127,127);  
-  drawSprites();
+	if(keyDown(LEFT_ARROW)||keyDown("A")) {
+		flower.rotation-=4;
+	}
+	if(keyDown(RIGHT_ARROW)||keyDown("D")) {
+		flower.rotation+=4;
+	}
+	
+	flower.rotation%=360;
+	
+	background(127,127,127);  
+	drawSprites();
 }
